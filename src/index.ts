b50367b358ab58468c846fe3e7e1e53f1c090056
peramalho <pedrorfig27@gmail.com>
2025-03-02 17:52:29 -0300

    Add morgan

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+app.use(morgan("common"));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`[server]: Server is running port ${port}`);
 });
