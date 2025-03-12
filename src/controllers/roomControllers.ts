c5f1a3f57540c68f07fa97b0b50706cf33292f06
peramalho <pedrorfig27@gmail.com>
2025-03-12 00:09:18 -0300

    feat: implement createRoom function

--- src/controllers/roomControllers.ts
+++ src/controllers/roomControllers.ts
@@ -1,8 +1,32 @@
-import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
 
-export function createRoom(req: Request, res: Response) {
+const prisma = new PrismaClient();
+
+export async function createRoom(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   try {
-  } catch {}
+    const hostId = req.params.hostId;
+
+    const host = await prisma.user.findUnique({
+      where: { id: req.params.hostId },
+    });
+
+    if (!host) {
+      res.status(404).json({ error: `User id not found: ${hostId}` });
+    }
+
+    const room = await prisma.room.create({
+      data: { hostId },
+    });
+
+    console.log("Room created:", room);
+  } catch (error) {
+    next(error);
+  }
 }
 
 export function getRoom(req: Request, res: Response) {}
