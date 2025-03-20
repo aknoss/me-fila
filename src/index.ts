edfc0ca1351c0b9639c9b78b24711b6a9d07d8d9
peramalho <pedrorfig27@gmail.com>
2025-03-20 19:19:20 -0300

    feat: add cors lib

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import morgan from "morgan";
 import { roomRoutes } from "./routes/roomRoutes";
 import { ApiResponse } from "./types";
@@ -8,6 +9,7 @@ import { logger } from "./logger";
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(
   morgan("tiny", {
     stream: { write: (message) => logger.info(message.trim()) },
