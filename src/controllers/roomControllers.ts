48c3bc5112bb3e36b16ec1a64e725a3f8a410caf
peramalho <pedrorfig27@gmail.com>
2025-04-10 22:50:49 -0300

    feat: add joinRoom handler

--- src/controllers/roomControllers.ts
+++ src/controllers/roomControllers.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { PrismaClient, Room } from "@prisma/client";
+import { PrismaClient, Room, User } from "@prisma/client";
 import { ApiResponse } from "../types";
 import { logger } from "../logger";
 import { getEnv } from "../env";
@@ -46,8 +46,8 @@ export async function createRoom(
 export async function getRoom(req: Request, res: Response) {}
 
 export async function deleteRoom(req: Request, res: ApiResponse) {
+  const roomId = req.roomId;
   try {
-    const roomId = req.roomId;
     await prisma.room.delete({ where: { id: roomId } });
     logger.info("Room deleted successfully");
     res.status(200).json({ data: null, error: null });
@@ -57,6 +57,44 @@ export async function deleteRoom(req: Request, res: ApiResponse) {
     res.status(404).json({
       data: null,
       error: { message: "Could not find room to delete", code: 404 },
+    });
+  }
+}
+
+type JoinRoomParams = { roomId: string };
+type JoinRoomResponse = ApiResponse<User>;
+export async function joinRoom(
+  req: Request<JoinRoomParams>,
+  res: JoinRoomResponse
+) {
+  const roomId = req.params.roomId;
+  const userId = req.userId;
+  try {
+    const room = await prisma.room.findUnique({
+      where: { id: roomId },
+    });
+
+    if (!room) {
+      logger.error("Could not find room to join", { roomId });
+      return res.status(404).json({
+        data: null,
+        error: { message: "Could not find room to join", code: 404 },
+      });
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: { participatedRoomId: roomId },
+    });
+
+    logger.error("User joined room successfully", { updatedUser });
+    res.status(200).json({ data: updatedUser, error: null });
+  } catch (err) {
+    const error = err instanceof Error ? err : new Error("Unknown error");
+    logger.error("Could not insert user into the room", { error });
+    res.status(500).json({
+      data: null,
+      error: { message: "Could not insert user into the room", code: 404 },
     });
   }
 }
