5bc29b3a220a5bdc48d9d84ec3ddcc5aec40df16
aknoss <pedrorfig27@gmail.com>
2026-02-19 20:05:10 -0300

    remove pg and prisma

--- server/prisma/migrations/20250302203342_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Room" (
-    "id" SERIAL NOT NULL,
-    "host" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
-);
