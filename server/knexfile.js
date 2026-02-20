70347d5c9a853aacf70912e77a101e676f287c77
aknoss <pedrorfig27@gmail.com>
2026-02-20 18:18:38 -0300

    convert string to number for port

--- server/knexfile.js
+++ server/knexfile.js
@@ -7,7 +7,7 @@ export default {
     client: "mysql2",
     connection: {
       host: process.env.DATABASE_HOST,
-      port: process.env.DATABASE_PORT,
+      port: Number(process.env.DATABASE_PORT),
       database: process.env.DATABASE_NAME,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
