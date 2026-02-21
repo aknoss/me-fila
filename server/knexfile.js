a2ceca5b05b91364c8feec96fbc5d7c0cd67a1cc
aknoss <pedrorfig27@gmail.com>
2026-02-21 12:03:29 -0300

    remove knex env, just keep one

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,21 +3,15 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export default {
-  production: {
-    client: "mysql2",
-    connection: {
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "./migrations",
-    },
+  client: "mysql2",
+  connection: {
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+  },
+  migrations: {
+    directory: "./migrations",
   },
 }
