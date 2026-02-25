74ebf811287b7525b296ac82663ba47d10c971cf
aknoss <pedrorfig27@gmail.com>
2026-02-25 19:12:29 -0300

    add shared types

---
+++ shared/types.ts
@@ -0,0 +1,27 @@
+export type Room = {
+  id: string;
+  name: string;
+};
+
+export type User = {
+  id: string;
+  name: string;
+  room_id: string | null;
+};
+
+export type ApiSuccessResponse<T> = {
+  data: T;
+  error: null;
+};
+
+export type ApiErrorResponse = {
+  data: null;
+  error: { message: string; code: number };
+};
+
+export type ApiResponse<T> = ApiSuccessResponse<T> | ApiErrorResponse;
+
+export enum Role {
+  HOST = "HOST",
+  USER = "USER",
+}
