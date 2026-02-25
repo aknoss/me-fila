ae53c4f2bd00d62bd5d76c408664948e3eb546bf
aknoss <pedrorfig27@gmail.com>
2026-02-25 18:59:58 -0300

    add schema types

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,5 +1,16 @@
 import type { Response } from "express"
 
+type Room = {
+  id: string
+  name: string
+}
+
+type User = {
+  id: string
+  name: string
+  room_id?: string
+}
+
 export type ApiResponse<T = null> = Response<{
   data: T | null
   error: { message: string; code: number } | null
